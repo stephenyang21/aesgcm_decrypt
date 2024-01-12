c805bb72784755f97e658d32ff4516b63ae879e3
Stephen Yang <stephenyang@ip-192-168-1-13.eu-west-2.compute.internal>
2024-01-12 13:20:14 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import binascii
+
+from decrypt import decrypt
+from hex_json import hex_serialize, hex_deserialize
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+
+st.title("AES-GCM  Decryption")
+
+st.header("Decryption")
+message = st.text_input("Enter the message to decrypt:")
+key = st.text_input("Enter Shared Key:")
+
+okButtom = st.button("OK")
+
+if okButtom:
+    decrypted_message = decrypt(message.encode('utf-8'), (key.encode('utf-8')))
+    data  =binascii.hexlify(decrypted_message).decode('utf-8')
+    st.subheader("Decrypted Message" )
+    st.write(data)
+    st.subheader("JSON Message:")
+    st.json(hex_deserialize(data))
+
+
+
+
+
